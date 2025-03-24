c50585f80ccb824b7d45d6c37d95bff7b88ae88e
Jokezor <testifyingfor@gmail.com>
2025-03-24 18:31:52 +0100

    Started on the scanner.

--- java/app/src/main/java/lox/Lox.java
+++ java/app/src/main/java/lox/Lox.java
@@ -13,6 +13,8 @@ import java.util.List;
 
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -28,6 +30,9 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -40,6 +45,7 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
@@ -55,4 +61,14 @@ public class Lox {
         */
         System.out.println("Hello! :)");
     }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where ": " + message);
+        hadError = true;
+    }
+
 }
---
+++ java/app/src/main/java/lox/Scanner.java
@@ -0,0 +1,21 @@
+package lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static lox.TokenType.*;
+
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner (String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+
+    }
+}
